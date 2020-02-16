f756996925b7949c60127e865001d89c26da6c1b
TheDomesticUser <TheDomesticUser@gmail.com>
2020-02-16 18:16:42 -0500

    Fixed some bugs and added some small changes

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import (TemplateView, ListView,
+                                    DetailView, CreateView, UpdateView, DeleteView)
+from django.urls import reverse
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'basic_app/index.html'
