befedf56142ab9721fa85c59b4cc0619502482fd
TheDomesticUser <TheDomesticUser@gmail.com>
2020-02-16 20:26:26 -0500

    Added post and comment model

--- basic_app/models.py
+++ basic_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# Regular users
 class User(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
@@ -8,10 +9,26 @@ class User(models.Model):
     def __str__(self):
         return self.username
 
-class Blog(models.Model):
-    commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
     message = models.CharField(max_length=2048)
     date_posted = models.DateField()
 
     def __str__(self):
         return self.commenter + ': ' + self.date_posted
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    post_commented = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_commented')
+    message = models.CharField(max_length=1024)
+    date_commented = models.DateField()
+
+    def __str__(self):
+        return self.author + ': ' + self.date_commented
+
+# Admins
+# class AdminPost(models.Model):
+#     post = models.OneToOneField(Post, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.blog.commenter
